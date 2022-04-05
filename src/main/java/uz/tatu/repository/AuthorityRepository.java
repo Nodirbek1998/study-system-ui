c48d081a2bf069e33189707beea65591dc644498
Nodirbek <nodirbekmamadaliyev1998@gmaiil.com>
2022-04-05 12:09:24 +0500

    Study system

--- src/main/java/uz/tatu/repository/AuthorityRepository.java
+++
@@ -1,9 +0,0 @@
-package uz.tatu.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import uz.tatu.domain.Authority;
-
-/**
- * Spring Data JPA repository for the {@link Authority} entity.
- */
-public interface AuthorityRepository extends JpaRepository<Authority, String> {}
